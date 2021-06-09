548a893a421c9cedadbe6d7c259e6b8b71896867
Atharva Deolalikar <atharvadeolalikar@gmail.com>
2021-06-10 00:46:11 +0530

    contact form success -fixed

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,5 +1,20 @@
-window.onload = function exampleFunction() {
-    if (document.URL.indexOf("email") > -1) {
-        document.getElementById('sent').classList.add('success');
+function init(){
+  var params = new URLSearchParams(window.location.search); 
+
+  if (params==''){
+      //Do nothing
+  }else{
+      if(params.has('success')){
+          document.getElementById('sent').style.display='block';
+          $('html, body').animate({
+            'scrollTop' : $("#sent").position().top-250
+        });
       }
+  }
+}
+
+const form = document.getElementById('form');
+
+window.onload = function() {
+  init();
 }
