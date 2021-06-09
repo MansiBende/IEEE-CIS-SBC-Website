e3eb397efbffb95f904e8fee4a4c8896b881b536
Atharva Deolalikar <atharvadeolalikar@gmail.com>
2021-06-10 02:03:17 +0530

    window onload fun fixed

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -13,6 +13,22 @@ function init(){
   }
 }
 
+function check(){
+  var params = new URLSearchParams(window.location.search); 
+
+  if (params==''){
+      //Do nothing
+  }else{
+      if(params.has('success')){
+          document.getElementById('newsletter-success').style.display='block';
+          $('html, body').animate({
+            'scrollTop' : $("#footer").position().top-200
+        });
+      }
+  }
+}
+
 window.onload = function() {
+  check();
   init();
 }
